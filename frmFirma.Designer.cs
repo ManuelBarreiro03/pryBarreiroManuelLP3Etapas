8ce1217b3411f670369c3e163f75e7936a116b80
ManuelBarreiro03 <manuelbarreiro2003@gmail.com>
2024-05-15 08:24:44 -0300

    Agregar archivos de proyecto.

---
+++ frmFirma.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace pryBarreiroManuelLP3Etapas
+{
+    partial class frmFirma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmdVolver = new Button();
+            cmdGrabar = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // cmdVolver
+            // 
+            cmdVolver.Location = new Point(93, 284);
+            cmdVolver.Name = "cmdVolver";
+            cmdVolver.Size = new Size(75, 23);
+            cmdVolver.TabIndex = 5;
+            cmdVolver.Text = "Volver";
+            cmdVolver.UseVisualStyleBackColor = true;
+            cmdVolver.Click += cmdVolver_Click;
+            // 
+            // cmdGrabar
+            // 
+            cmdGrabar.Location = new Point(12, 284);
+            cmdGrabar.Name = "cmdGrabar";
+            cmdGrabar.Size = new Size(75, 23);
+            cmdGrabar.TabIndex = 4;
+            cmdGrabar.Text = "Grabar";
+            cmdGrabar.UseVisualStyleBackColor = true;
+            cmdGrabar.Click += cmdGrabar_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(493, 266);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            // 
+            // frmFirma
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(517, 318);
+            Controls.Add(cmdVolver);
+            Controls.Add(cmdGrabar);
+            Controls.Add(pictureBox1);
+            Name = "frmFirma";
+            Text = "frmFirma";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button cmdVolver;
+        private Button cmdGrabar;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ frmFirma.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryBarreiroManuelLP3Etapas
+{
+    public partial class frmFirma : Form
+    {
+        public Bitmap archivoImagen;
+        public frmFirma()
+        {
+            InitializeComponent();
+            archivoImagen = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString();
+            //dibujo que vaya poniendo puntos 
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics g = Graphics.FromImage(archivoImagen))
+                {
+                    g.FillEllipse(Brushes.Black, e.X, e.Y, 5, 5);
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(archivoImagen, Point.Empty);
+        }
+
+        private void cmdGrabar_Click(object sender, EventArgs e)
+        {
+            string fechaDia = "", fechaHora = "", fechaMes = "", fechaAno = "", fechaMinutos = "", FechaHoy = "";
+            fechaDia = DateTime.Now.Day.ToString();
+            fechaHora = DateTime.Now.Hour.ToString();
+            fechaMes= DateTime.Now.Month.ToString();
+            fechaAno = DateTime.Now.Year.ToString();
+            fechaMinutos = DateTime.Now.Minute.ToString();
+            FechaHoy = fechaAno+ "." + fechaMes + "." + fechaDia + " " + fechaHora + "." + fechaMinutos;
+            archivoImagen.Save(FechaHoy + ".jpg"); 
+        }
+
+        private void cmdVolver_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmPrincipal Principal = new frmPrincipal();
+            Principal.ShowDialog();
+        }
+    }
+}
